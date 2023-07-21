da254af182d4657fe7cecbb3ad354f7221107de8
Abdul Rehman <abdulrehman19761@gmail.com>
2023-07-21 22:01:12 +0500

    appjs code clean

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-
 function App() {
   let [state, setState] = useState({
     string: "",
@@ -8,17 +7,12 @@ function App() {
     stringCount: 0,
     method: "Object",
   });
-  let bc = useRef(new BroadcastChannel("test_memory"));
 
   let worker = useRef("");
-
   let [tabs, setTabs] = useState([]);
-  let [ids, setIds] = useState([]);
 
   useEffect(() => {
-    // console.log(" worker", worker.current);
     worker.current = new SharedWorker("worker.js");
-    // worker.current.port.postMessage("app");
     worker.current.port.onmessage = function (e) {
       console.log("Message received from worker", e);
       switch (e.data.type) {
@@ -46,7 +40,6 @@ function App() {
       }
       // console.log("e", e);
     };
-    // worker.port.start();
     const updateInfo = ({ id, prop }) => {
       setTabs((updateTabs) => {
         return updateTabs.map((tab) => {
@@ -58,115 +51,18 @@ function App() {
         });
       });
     };
-    // // console.log("something...", ids);
-    // bc.current.onmessage = (e) => {
-    //   // The data type of the tab is a tab created by the user.
-    //   switch (e.data.type) {
-    //     case "createTab":
-    //       setState((preState) => {
-    //         let { string, fileCount, stringCount, method } = preState;
-    //         bc.current.postMessage({
-    //           type: "startProcess",
-    //           ids,
-    //           inputstring: string,
-    //           fileCount,
-    //           stringCount,
-    //           method,
-    //         });
-    //         return preState;
-    //       });
-    //       setTabId(e.data.id);
-    //       setIds((preIds) => [...preIds, e.data.id]);
-    //       break;
-    //     // case "createTabHandler":
-    //     //   // createTab(e.data);
-    //     //   debugger;
-    //     //   console.log("trigerr");
-    //     //   break;
-    //     case "updateProps":
-    //       debugger;
-    //       handleChangeProps(e.data);
-    //       break;
-    //     case "nettime":
-    //       netTimeSet(e.data);
-    //       break;
-    //     case "tabClosed":
-    //       handleTabClosed(e.data);
-    //       break;
 
-    //     default:
-    //       break;
-    //   }
-    // };
-
-    // eslint-disable-next-line no-use-before-define, react-hooks/exhaustive-deps
   }, []);
-  const createTab = ({ props }) => {
-    let { id } = props;
-    setIds((preIds) => [...preIds, id]);
-    setTabs((pretabs) => [...pretabs, props]);
-  };
-  const handleChangeProps = ({ props, id }) => {
-    // setTabId(e.data.id);
 
-    let updateTabs;
-    if (tabs.length > 0) {
-      updateTabs = tabs.map((tab) => {
-        // Returns a new tab with the same id as the tab.
-        if (tab.id === id) return { ...tab, ...props };
-        setTabs((preTbas) => [...preTbas, { id: id, ...props }]);
-        return tab;
-      });
-    } else {
-      updateTabs = [props];
-    }
 
-    setTabs(updateTabs);
-  };
-
-  const handleTabClosed = ({ id }) => {
-    let updateTabs = tabs.map((tab) => {
-      // Returns a new tab with the same id as the tab.
-      if (tab.id === id) return { ...tab, status: "closed" };
-      return tab;
-    });
-    setTabs(updateTabs);
-  };
-  const netTimeSet = ({ id, netTime }) => {
-    let updateTabs = tabs.map((tab) => {
-      // Returns a new tab with the same id as the tab.
-      if (tab.id === id) return { ...tab, netTime: netTime };
-      return tab;
-    });
-    setTabs(updateTabs);
-  };
-  const setTabId = (id) => {
-    debugger;
-    setTabs((preTabs) => [...preTabs, { id: id }]);
-    // console.log("tabs", tabs);
-  };
 
   const handleSaveFileCount = (e) => {
     e.preventDefault();
-    // let { string, fileCount, stringCount, method } = state;
     // count the number of items in the oldtab
     window.open("/newTab", "_blank", "noopener");
-    // setTimeout(() => {
-    //   worker.current.port.postMessage({
-    //     type: "startProcess",
-    //     state,
-    //   });
-    // }, 1000);
-    // setTimeout(() => {
-
-    // }, 1500);
   };
   const handleCloseTab = (id) => {
-    // setTabs(newlist);
-    // tab.close();
     tabs.forEach((oldtab) => {
-      // oldtab.onload = () => {
-      // Close the oldtab. id if oldtab. id is oldtab. id
       if (oldtab.id === id) {
        worker.current.port.postMessage({
           type: "close",
